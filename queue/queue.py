edf699ecb2b22d03724ff4b3aef80da096b24950
Tristan Grovender <therealtristangrovender@gmail.com>
2019-08-21 17:57:35 -0500

    Implemented enqueue, dequeue, and len in queue with all tests passing

--- queue/queue.py
+++ queue/queue.py
@@ -3,13 +3,20 @@ class Queue:
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    self.storage = []
 
-  def enqueue(self, item):
-    pass
+  def enqueue(self, item): # adds item to the back of the queue.
+    self.storage.insert(0,item)
+    self.size += 1
   
-  def dequeue(self):
-    pass
 
-  def len(self):
-    pass
+  def dequeue(self): # Removes and returns an item from the front of the queue.
+    if self.size == 0:
+      return None
+    else:
+      self.size -= 1
+      return self.storage.pop()
+
+
+  def len(self): # returns the number of items in the queue
+    return self.size
