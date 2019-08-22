3ad0cc0964f55dad58367dafe958675388d75466
Tristan Grovender <therealtristangrovender@gmail.com>
2019-08-22 10:25:18 -0500

    binary search tree, queue, and DLL tests passing. Working on lru_cache.

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,33 +1,51 @@
+import sys
+sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+"""
+Our LRUCache class keeps track of the max number of nodes it
+can hold, the current number of nodes it is holding, a doubly-
+linked list that holds the key-value entries in the correct 
+order, as well as a storage dict that provides fast access
+to every node stored in the cache.
+"""
 class LRUCache:
-  """
-  Our LRUCache class keeps track of the max number of nodes it
-  can hold, the current number of nodes it is holding, a doubly-
-  linked list that holds the key-value entries in the correct 
-  order, as well as a storage dict that provides fast access
-  to every node stored in the cache.
-  """
   def __init__(self, limit=10):
-    pass
-
+    self.limit = limit
+    self.size = 0
+    self.order = DoublyLinkedList()
+    self.storage = dict()
   """
-  Retrieves the value associated with the given key. Also
-  needs to move the key-value pair to the end of the order
-  such that the pair is considered most-recently used.
-  Returns the value associated with the key or None if the
-  key-value pair doesn't exist in the cache. 
+  Retrieves the value of the node given the key. Moves the
+  retrieved node to the end of self.order. Should be an 
+  O(1) operation.
   """
   def get(self, key):
-    pass
-
+    if key in self.storage:
+      node = self.storage[key]
+      self.order.move_to_end(node)
+      return node.value[1]
+    else:
+      return None
   """
-  Adds the given key-value pair to the cache. The newly-
-  added pair should be considered the most-recently used
-  entry in the cache. If the cache is already at max capacity
-  before this entry is added, then the oldest entry in the
-  cache needs to be removed to make room. Additionally, in the
-  case that the key already exists in the cache, we simply 
-  want to overwrite the old value associated with the key with
-  the newly-specified value. 
+  Sets the given key-value pair as the new tail of self.order.
+  Also adds the key-value pair to the self.storage. If 
+  self.order is already holding the max number of pairs, the
+  head of self.order will need to be evicted before the new
+  key-value pair is added. Lastly, if the key already exists
+  in the cache, the old value of the key should be updated, and
+  the newly-updated key-value pair should then be moved to the
+  end of self.order. Should be an O(1) operation.
   """
   def set(self, key, value):
-    pass
+    if key in self.storage:
+      node = self.storage[key]
+      node.value = (key, value)
+      self.order.move_to_end(node)
+      return
+    if self.size == self.limit:
+      del self.storage[self.order.head.value[0]]
+      self.order.remove_from_head()
+      self.size -= 1
+    self.order.add_to_tail((key, value))
+    self.storage[key] = self.order.tail
+    self.size += 1
--- queue/queue.py
+++ queue/queue.py
@@ -1,21 +1,24 @@
+from doubly_linked_list import DoublyLinkedList
+
 class Queue:
   def __init__(self):
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = []
+    self.storage = DoublyLinkedList()
 
   def enqueue(self, item): # adds item to the back of the queue.
-    self.storage.insert(0,item)
+    self.storage.add_to_tail(item)
     self.size += 1
   
 
   def dequeue(self): # Removes and returns an item from the front of the queue.
-    if self.size == 0:
-      return None
-    else:
+    if self.size > 0:
       self.size -= 1
-      return self.storage.pop()
+    else:
+      return None
+
+    return self.storage.remove_from_head()
 
 
   def len(self): # returns the number of items in the queue
